7e70196ebd97964e319458750a506e7c66904b12
doramaidink <vqthanh1999@gmail.com>
2025-11-07 07:53:48 +0700

    Xóa các file/thư mục không cần thiết

--- src/routes/chodadat.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const chodadatController = require('../app/controllers/chodadatController');
-
-
-router.get('/chodadatnguoidung', chodadatController.home);
-
-module.exports = router;
